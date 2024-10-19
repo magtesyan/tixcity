9f4d6616ce1f5140a4ac1294d27ccf426100d93e
Yury-Sergeevich <yury.kravchenko.1987@gmail.com>
2024-10-19 12:58:03 +0300

    Added dinamic-background in categories

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,87 @@
 <template>
   <div class="wrapper">
-    <Header />
-    <slot />
-    <Footer />
+    <div :class="backgroundClass">
+      <Header />
+      <slot />
+      <Footer />
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const category = route.params.categories;
+const backgroundClass = computed(() => {
+  switch (category) {
+    case "theaters":
+      return "bg-theaters";
+    case "kids":
+      return "bg-kids";
+    case "sport":
+      return "bg-sport";
+    case "stage":
+      return "bg-stage";
+    case "opera":
+      return "bg-opera";
+    default:
+      return "bg-default";
+  }
+});
+</script>
 
 <style lang="scss">
 .wrapper {
   height: 100vh;
   width: 100%;
-  background-image: url(../public/img/screen-background/background.png);
+}
+
+.bg-theaters {
+  background-image: url("/public/img/screen-background/theater.png");
   background-repeat: no-repeat;
   background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+.bg-kids {
+  background-image: url("/public/img/screen-background/bg-cirqe.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+.bg-sport {
+  background-image: url("/public/img/screen-background/bg-sport.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+.bg-stage {
+  background-image: url("/public/img/screen-background/bg-stage.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+.bg-opera {
+  background-image: url("/public/img/screen-background/bg-opera.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+.bg-default {
+  background-image: url("/public/img/screen-background/default.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
 }
 </style>
