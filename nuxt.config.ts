e94eae663224a418cc8fdcc45101f48d06d3e26f
Mikael Magtesyan <magtesyan@inbox.ru>
2024-11-23 21:42:10 +0300

    Fix duplicating styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  css: ["@/assets/scss/main.scss"],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/main.scss" as *;',
+          additionalData: `
+            @import "@/assets/scss/variables.scss";
+            @import "@/assets/scss/mixins.scss";`,
         },
       },
     },
